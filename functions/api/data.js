24159572bf680e5f03849e8bc86e59e1a92092be
yukfokusid-art <yukfokus.id@gmail.com>
2026-05-12 15:44:11 +0700

    data.js

--- functions/api/data.js
+++ functions/api/data.js
@@ -1,21 +1,25 @@
 export async function onRequest(context) {
   const { request, env } = context;
   const KV = env.PRODUK_DB;
+  const url = new URL(request.url);
+  const username = url.searchParams.get("u");
+
+  if (!username) return new Response("User Required", { status: 400 });
+
+  const STORAGE_KEY = `products_${username}`;
 
   if (request.method === "GET") {
-    const data = await KV.get("all_products", "json") || {};
+    const data = await KV.get(STORAGE_KEY, "json") || {};
     return Response.json(data);
   }
 
   if (request.method === "POST") {
     try {
       const data = await request.json();
-      await KV.put("all_products", JSON.stringify(data));
+      await KV.put(STORAGE_KEY, JSON.stringify(data));
       return Response.json({ success: true });
     } catch (error) {
-      return Response.json({ success: false, error: error.message }, { status: 500 });
+      return Response.json({ success: false }, { status: 500 });
     }
   }
-
-  return new Response("Method Not Allowed", { status: 405 });
 }
